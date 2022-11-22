a3bf08774193affda62454a1fd814b9b6c9b7c74
GaneshGotame <83527532+GaneshGotame@users.noreply.github.com>
2022-11-22 18:28:45 +0545

    Add files via upload

---
+++ day31/script.js
@@ -0,0 +1,20 @@
+function c1(){
+    document.getElementById('bubble').style.background='red';
+}
+function c2(){
+    document.getElementById('bubble').style.background='green';
+}
+function c3(){
+    document.getElementById('bubble').style.background='blue';
+}
+function c4(){
+    document.getElementById('bubble').style.background='white';
+}
+function c5(){
+    document.getElementById('bubble').style.background='orange';
+}
+
+function speedup(){
+    const fast=document.getElementById('bubble');
+    fast.classList.toggle("fast");
+}
